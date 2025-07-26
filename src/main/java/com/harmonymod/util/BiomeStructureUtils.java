382c2f74a9c9b36fa0c9a8609b98efca62aa9f0f
restary259 <ranimdwahde3@gmail.com>
2025-07-26 14:28:37 +0300

    Update BiomeStructureUtils.java

--- src/main/java/com/harmonymod/util/BiomeStructureUtils.java
+++ src/main/java/com/harmonymod/util/BiomeStructureUtils.java
@@ -5,20 +5,14 @@ import net.minecraft.resources.ResourceLocation;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Utility methods for working with biomes and structures in Harmony API.
- */
 public class BiomeStructureUtils {
-
-    /**
-     * Convenience method to create a set of ResourceLocations from string IDs.
-     * Example: resourceSet("minecraft:plains", "mymod:cool_biome")
-     */
     public static Set<ResourceLocation> resourceSet(String... ids) {
         Set<ResourceLocation> set = new HashSet<>();
-        for (String id : ids) {
-            set.add(new ResourceLocation(id));
-        }
+        for (String id : ids) set.add(new ResourceLocation(id));
         return set;
+    }
+
+    public static boolean isVanilla(String id) {
+        return id != null && id.startsWith("minecraft:");
     }
 }
